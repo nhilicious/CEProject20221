4ed7f5965a52f8261de993a589649a5143df4756
nhilicious <nhi.pt205190@sis.hust.edu.vn>
2023-02-11 23:15:07 +0700

    ASK modulation and group folder

--- ASK/ASK_Demodulation_With_Noise.m
+++ ASK/ASK_Demodulation_With_Noise.m
@@ -1,22 +1,20 @@
 % <<<<<<<<<<<<<<<<<<<< ASK Modulation and Demodulation >>>>>>>>>>>>>>>>>>>>
 
 clc, clear all, close all;
-% ******************* Digital/Binary input information ********************
 
-% x = input('Enter Digital Input Information = ');   % Binary information as stream of bits (binary signal 0 or 1)
-x = randi(2, [1,10000], 'int32') - 1; % auto generate the binary sequence
-x1 = x;
-N = length(x);
-Tb = 0.0001;   %Data rate = 1MHz i.e., bit period (Thoi gian truyen 1bit)
+% ******************* Digital/Binary input information ********************
+num_bit = 1000;
+x = randi(2, [1,num_bit], 'int32') - 1; % Randomly generate a Binary information as stream of bits (binary signal 0 or 1)
+x1 = x; % x1 stores the input binary signal 
+N = length(x); % length of the binary information
+Tb = 0.001;   % one bit lasts in Tb second or bit rate = 1/Tb
 Rb = 1/Tb; % Rb = bit rate 
-% disp('Binary Input Information at Transmitter: ');
-% disp(x);
+
 
 % ************* Represent input information as digital signal *************
 
 % Tb: Thời gian truyền 1 bit
-% nb: 1 bit được biểu diễn bởi nb signal (số mẫu lấy trong 1 chu kì truyền
-% 1 bit)
+% nb: 1 bit được biểu diễn bởi nb signal (số mẫu lấy trong 1 chu kì truyền 1 bit)
 % Tb/nb: thời gian truyền một signal
 
 nb = 100;   % Digital signal per bit
@@ -30,8 +28,9 @@ for n = 1:1:N
      digit = [digit sig];
 end
 
-t1 = Tb/nb : Tb/nb : nb*N*(Tb/nb);   % Time period
-figure('Name','ASK Modulation and Demodulation With Noise','NumberTitle','off');
+% plot the input binary information
+t1 = Tb/nb : Tb/nb : nb*N*(Tb/nb);   % time axis
+figure('Name','2-ASK Modulation and Demodulation With Noise','NumberTitle','off');
 subplot(7,1,1);
 plot(t1,digit,'LineWidth',2.5);
 grid on;
@@ -41,32 +40,39 @@ ylabel('Amplitude(Volts)');
 title('Digital Input Signal');
 
 % *************************** ASK Modulation *****************************
-Ac1 = 5;     % Carrier amplitude for binary input '1'
+Eb = 15; % energy per bit (V^2)
+
+% 100% depth BASK unipolar modulation
+Ac1 = 2*sqrt(Eb/Tb);     % Carrier amplitude for binary input '1'
 Ac2 = 0;      % Carrier amplitude for binary input '0'
-br = 1/Tb;    % Bit rate
-Fc = br*10;   % Carrier frequency --> tai sao = 10* bit_rate 
-t2 = Tb/nb:Tb/nb:Tb;   % Signal time
+
+% 50% depth BASK 
+% A = 4* sqrt(Eb/ (5*Tb));
+% Ac1 = A;    % Carrier amplitude for binary input '1'
+% Ac2 = A/2;      % Carrier amplitude for binary input '0'
+
+Fc = Rb*10;   % Carrier frequency
 mod = [];
 
-x_ = Tb/nb:Tb/nb:Tb*N; % Time period
-y1 = Ac1*cos(2*pi*Fc*x_); % carrier function 1
-y2 = Ac2*cos(2*pi*Fc*x_); % carrier function 2
+y1 = Ac1*cos(2*pi*Fc*t1); % carrier function 1
+y2 = Ac2*cos(2*pi*Fc*t1); % carrier function 2
 
 % plot carrier 1
 subplot(7,1,2);
-plot(x_,y1);
+plot(t1,y1);
 xlabel('Time(Sec)');
 ylabel('Amplitude(Volts)');
 title('2-ASK Carrier 1');
 
 % plot carrier 2
 subplot(7,1,3);
-plot(x_,y2);
+plot(t1,y2);
 ylim([-20,20]);
 xlabel('Time(Sec)');
 ylabel('Amplitude(Volts)');
 title('2-ASK Carrier 2');
 
+t2 = Tb/nb:Tb/nb:Tb;   % Signal time
 for (i = 1:1:N)
     if (x(i) == 1)
         y = Ac1*cos(2*pi*Fc*t2);   % Modulation signal with carrier signal 1
@@ -76,9 +82,9 @@ for (i = 1:1:N)
     mod = [mod y];
 end
 
-t3 = Tb/nb:Tb/nb:Tb*N;   % Time period
+% plot modulated signal
 subplot(7,1,4);
-plot(t3,mod);
+plot(t1,mod);
 grid on;
 ylim([-20,20]);
 xlabel('Time(Sec)');
@@ -88,48 +94,37 @@ title('ASK Modulated Signal');
 % ********************* Transmitted signal x ******************************
 x = mod;
 
-% ********************* Channel model h and w *****************************
-h = 1;   % Signal fading 
-N0 = 50; % (W/Hz)
-% w = sqrt(N0/2) * rand(1, N*nb);   % Noise
-mean = 0;
-sigma = sqrt(N0/2);
-w = randn(size(x)); % Generate random numbers from a standard normal distribution
-w = mean + sigma*w; % Scale the random numbers to obtain Gaussian distribution with mean and sigma
-% display(w);
+% ********************* Channel model h and noise *****************************
+%h = 1;   % Signal fading 
 
 % ********************* Received signal y *********************************
-disp(size(x));
-% Received signal without noise
-y1 = x;
-t3 = Tb/nb:Tb/nb:Tb*N;   % Time period
-subplot(7,1,5);
-plot(t3,y1);
-grid on;
-xlabel('Time(Sec)');
-ylabel('Amplitude(Volts)');
-title('ASK Received Signal Without Noise');
-% -------------------------------------------------------------------------
-% Received signal with white noise
-y = h.*x + w;   % add noise
-t3=Tb/nb:Tb/nb:Tb*N;   % Time period
-subplot(7,1,6);
-plot(t3,y);
-grid on;
-xlabel('Time(Sec)');
-ylabel('Amplitude(Volts)');
-title('ASK Received Signal With White Noise');
 
+% Received signal with white noise
+N0 = 1; % (W/Hz)
+mean = 0;
+sigma = sqrt(N0/2);
+noise = randn(size(x)); % Generate random numbers from a standard normal distribution
+noise = mean + sigma*noise; % Scale the random numbers to obtain Gaussian distribution with mean and sigma
+% display(noise);
+y = x + noise;   % add noise
+
+% plot Received signal with white noise
+subplot(7,1,6);
+plot(t1,y);
+grid on;
+xlabel('Time(Sec)');
+ylabel('Amplitude(Volts)');
+title('2-ASK Received Signal With White Noise');
 % *************************** ASK Demodulation ****************************
-s = length(t2); % số cặp giá trị (t,y) để biểu diễn 1 tín hiệu
+s = length(t2);
 demod = [];
 for n = s:s:length(y) 
     t4 = Tb/nb:Tb/nb:Tb;    % Time period
-    c = cos(2*pi*Fc*t4);    % Carrier signal % basis of M
+    c = cos(2*pi*Fc*t4);    % basis of M
     mm = c.*y((n-(s-1)):n); % Convolution 
     t5 = Tb/nb:Tb/nb:Tb;
     z = trapz(t5,mm);       % Integration 
-    rz = 2*z/Tb; % khong hieu sao phải *2/Tb
+    rz = 2*z/Tb;
     d1 = abs(rz-Ac1);
     d2 = abs(rz-Ac2);
     if (d1 < d2)
@@ -139,11 +134,11 @@ for n = s:s:length(y)
     end
     demod = [demod a];
 end
+
 %disp('Demodulated Binary Information at Receiver: ');
 %disp(demod);
 
 % ********** Represent demodulated information as digital signal **********
-
 
 digit = [];
 for n = 1:length(demod);
@@ -156,20 +151,26 @@ for n = 1:length(demod);
 end
 
 t5 = Tb/nb:Tb/nb:nb*length(demod)*(Tb/nb);   % Time period
+
 subplot(7,1,7)
 plot(t5,digit,'LineWidth',2.5);grid on;
 axis([0 Tb*length(demod) -0.5 1.5]);
 grid on;
 xlabel('Time(Sec)');
 ylabel('Amplitude(Volts)');
-title('ASK Demodulated Binary Data');
+title('2-ASK Demodulated Binary Data');
 
-% Calculate BER
+% *************************** Calculate BER *******************************
 num = xor(demod, x1);
-fprintf('Compare input signal and received signal:\n');
-%disp(num);
-fprintf('BER = %i\n', sum(num));
+fprintf('*** Compare input signal and received signal ***\n');
+fprintf('Number of binary information bit: %d\n', num_bit);
+fprintf('Number of incorrect bit = %i\n', sum(num));
+fprintf('BER = %.4f \n', sum(num)/num_bit);
+
+fprintf('\n*** BER Theory ***\n')
+fprintf('Eb/N0 = %f\n',Eb/N0);
+fprintf('10*lg(Eb/N0) = %f\n',10*log10(Eb/N0));
+fprintf('BASK 50%% -->\t P(e) = %f\n', 0.5 * erfc(sqrt(Eb/(4*N0)))); % for BASK - 50
+fprintf('BASK 100%% -->\t P(e) = %f\n', 0.5 * erfc(sqrt(Eb/(2*N0)))); % for BASK - 100
 
 % ************************** End of the program ***************************
-
-
